1a630a6c5f69f6370b383b6cbd19e0db26c0ac97
Julian Martinez <73849597+Julian-dev28@users.noreply.github.com>
2024-10-10 22:23:51 +0000

    update comments; remove unused code

--- src/Navigation.tsx
+++ src/Navigation.tsx
@@ -1,13 +1,13 @@
-import React, { useState } from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link, useLocation } from "react-router-dom";
 
 const Navigation: React.FC = () => {
   const location = useLocation();
   const [hoverIndex, setHoverIndex] = useState<number | null>(null);
 
   const navItems = [
-    { path: '/', label: 'Home' },
-    { path: '/widget', label: 'DEX Widget' }
+    { path: "/", label: "Home" },
+    { path: "/widget", label: "DEX Widget" },
   ];
 
   return (
@@ -17,12 +17,14 @@ const Navigation: React.FC = () => {
       </div>
       <ul className="nav-list">
         {navItems.map((item, index) => (
-          <li key={item.path} 
-              onMouseEnter={() => setHoverIndex(index)}
-              onMouseLeave={() => setHoverIndex(null)}>
-            <Link 
-              to={item.path} 
-              className={location.pathname === item.path ? 'active' : ''}
+          <li
+            key={item.path}
+            onMouseEnter={() => setHoverIndex(index)}
+            onMouseLeave={() => setHoverIndex(null)}
+          >
+            <Link
+              to={item.path}
+              className={location.pathname === item.path ? "active" : ""}
             >
               {item.label}
               {(hoverIndex === index || location.pathname === item.path) && (
@@ -32,11 +34,6 @@ const Navigation: React.FC = () => {
           </li>
         ))}
       </ul>
-      {/* <div className="nav-chain">
-        {[...Array(5)].map((_, i) => (
-          <div key={i} className="chain-link" style={{animationDelay: `${i * 0.1}s`}} />
-        ))}
-      </div> */}
     </nav>
   );
 };

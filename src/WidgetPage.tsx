852ed7a6776e1b56a4b33d69cdd8dc31232e72bc
Julian Martinez <73849597+Julian-dev28@users.noreply.github.com>
2024-11-05 02:00:39 +0000

    fix typo

--- src/WidgetPage.tsx
+++ src/WidgetPage.tsx
@@ -38,7 +38,7 @@ const WidgetPage: React.FC = () => {
         fromToken: "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee", // OKB
         toToken: "0x74b7f16337b8972027f6196a17a631ac6de26d22", // USDC
       },
-      bridgeTokenPar: {
+      bridgeTokenPair: {
         fromChain: 8453, // BASE
         toChain: 196, // X Layer
         fromToken: "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee", // ETH

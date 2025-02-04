ea5c7cd79cbfc96f039efe9a9e6e35187755b1b5
MoscowFX <100240680+MoscowFX@users.noreply.github.com>
2025-02-04 19:19:10 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App: React.FC = () => {
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/widget" element={<WidgetPage />} />
-          <Route path="/newpage2" element={<newpage2 />} /> 
+          <Route path="/NewPage2" element={<NewPage2 />} /> 
         </Routes>
       </div>
     </Router>

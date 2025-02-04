501d5df94fffe87be9d07ee407982d0313583ec8
MoscowFX <100240680+MoscowFX@users.noreply.github.com>
2025-02-04 19:17:55 +0300

    Update newpage2.tsx

--- src/newpage2.tsx
+++ src/newpage2.tsx
@@ -1,92 +1,13 @@
-import React, { useRef, useEffect } from "react";
-import {
-  createOkxSwapWidget,
-  ProviderType,
-  OkxSwapWidgetProps,
-  OkxEvents,
-  OkxEventHandler,
-  IWidgetConfig,
-  TradeType,
-  THEME,
-} from "@okxweb3/dex-widget";
+import React from "react";
 
-declare global {
-  interface Window {
-    ethereum?: any;
-    solana?: any;
-  }
-}
-
-const WidgetPage: React.FC = () => {
-  const widgetRef = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    if (!widgetRef.current) return;
-
-    const params: OkxSwapWidgetProps["params"] = {
-      appCode: "YOUR_APP_CODE",
-      width: 450,
-      height: 400,
-      providerType: ProviderType.EVM,
-      tradeType: TradeType.AUTO,
-      chainIds: ["196", "8453"],
-      theme: THEME.LIGHT,
-      lang: "en_us",
-      tokenPair: {
-        fromChain: 196, //X Layer
-        toChain: 196, // X Layer
-        fromToken: "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee", // OKB
-        toToken: "0x74b7f16337b8972027f6196a17a631ac6de26d22", // USDC
-      },
-      bridgeTokenPair: {
-        fromChain: 8453, // BASE
-        toChain: 196, // X Layer
-        fromToken: "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee", // ETH
-        toToken: "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee", // OKB
-      },
-      feeConfig: {
-        196: {
-          feePercent: 1,
-          referrerAddress: "0xd37268a16374d0a52c801c06a11ef32a35fcd2b9",
-        },
-        8453: {
-          feePercent: 1,
-          referrerAddress: "0xd37268a16374d0a52c801c06a11ef32a35fcd2b9",
-        },
-      },
-    };
-    const provider = window.ethereum;
-
-    const listeners: IWidgetConfig["listeners"] = [
-      {
-        event: OkxEvents.ON_CONNECT_WALLET,
-        handler: (() => {
-          provider?.enable();
-        }) as OkxEventHandler<OkxEvents.ON_CONNECT_WALLET>,
-      },
-    ];
-
-    const widgetProps: IWidgetConfig = {
-      params,
-      provider,
-      listeners,
-    };
-
-    const instance = createOkxSwapWidget(widgetRef.current, widgetProps);
-
-    return () => {
-      instance.destroy();
-    };
-  }, []);
-
+const NewPage2: React.FC = () => {
   return (
-    <div className="widget-page">
-      
-      <h2>OKX DEX Widget2</h2>
-      <h4>попытка изменить код и добавить еще страницу</h4>
-      <div ref={widgetRef} />
+    <div>
+      <h4>Добро пожаловать на NewPage2</h4>
+      <h4>Здесь просто текст без виджета</h4>
+      <h4>Вы можете добавить сюда любую информацию</h4>
     </div>
   );
 };
 
-export default newpage2;
+export default NewPage2;
